30355b2fd65db08bb1a6b83ebb0dd652b7b530e3
Esteban Valente <87546415+valentesteban@users.noreply.github.com>
2021-08-16 00:45:43 -0300

    Change a little thing

--- linux/tea.sh
+++ linux/tea.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm ~/.var/app/com.jetbrains.*/config/JetBrains/*/eval/idea*.evaluation.key
-echo "The JetBrains licence has been restored. Thank you for using Tea!"
+echo 'The JetBrains licence has been restored. Thank you for using Tea!'
